9cb85a3105c8ecc2217785279cb4bcf119c315cb
Solayman <solayman.b@live.fr>
2021-11-03 12:29:14 +0100

    ajout des boutons de demande et de création de critiques

--- LITReview/LITReview/settings.py
+++ LITReview/LITReview/settings.py
@@ -127,3 +127,5 @@ STATICFILES_DIRS = [BASE_DIR / "LITReview/static"]
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'members.User'
--- LITReview/members/views.py
+++ LITReview/members/views.py
@@ -1,29 +1,26 @@
 from django.contrib.auth import authenticate, login
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from LITReview import forms
 from LITReview.forms import authentification_form
 
 
 def index(request):
 	login_form = forms.authentification_form()
-	message = ''
 	if request.method == 'POST':
-		print(request.POST)
 		login_form = forms.authentification_form(request.POST)
 		if login_form.is_valid():
-			print('hello1')
 			user = authenticate(
 				username=login_form.cleaned_data['username'],
 				password=login_form.cleaned_data['password1'],
 			)
-			print(user)
 			if user is not None:
 				login(request, user)
-				message = 'connexion réussie'
+				return redirect('/content/flux')
 			else:
-				message = 'Erreur saisie invalide'
+				messages.info(request, 'Identifiant et/ou mot de passe invalide(s)')
 	return render(
-		request, 'members/index.html', context={'form': login_form, 'message': message}
+		request, 'members/index.html', context={'form': login_form}
 	)
 
 def inscription(request):
